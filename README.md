b300d03304999fc7df1f2f839478262e92c706a9
rriifftt <rriifftt@gmail.com>
2019-04-20 03:21:30 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# parallel-ping
+
+Parallel Ping to Multiple targets.
+
+# Command Line Usage
+
+```
+Usage: parallel_ping.py [OPTIONS]
+
+Options:
+  -t, --target TEXT
+  -T, --timeout INTEGER
+  -c, --count INTEGER
+  --max-workers INTEGER
+  --help                 Show this message and exit.
+```
---
+++ parallel_ping.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import click
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import os
+from subprocess import run
+
+
+class ParallelPing(object):
+    """ do parallel ping """
+    def __init__(self, **kwargs):
+        self.targets = kwargs['targets']
+        self.retry_count = kwargs['count']
+        self.max_workers = kwargs['max_workers']
+        self.sysname = self.get_sysname()
+        self.ping_args = self.generate_ping_args(kwargs)
+        self.results = None
+
+    def get_sysname(self):
+        sysname = os.uname().sysname
+        supported_os = ['Darwin', 'Linux']
+
+        if sysname in supported_os:
+            return sysname
+        else:
+            raise NotImplementedError('Unsupported OS')
+
+    def generate_ping_args(self, kwargs):
+        """
+        generate ping options. supporting count and timeout.
+       
+        rtype: list
+        """
+        try:
+            timeout = str(kwargs['timeout'])
+        except KeyError:
+            raise
+
+        if self.sysname == 'Darwin':
+            return ['-t', timeout]
+        elif self.sysname == 'Linux':
+            return ['-W', timeout]
+
+    def get_ping_result(self, target):
+        """
+        ping to single target
+        """
+        command = ['ping', target]
+        command.extend(self.ping_args)
+
+        count = 0
+        while count < self.retry_count:
+            res = run(command, capture_output=True)
+            if res.returncode == 0:
+                return res
+            else:
+                count += 1
+        return res
+
+    def run(self):
+        """
+        parallel ping using concurrent.futures.TheadPoolExecutor
+        """
+        self.results = []
+        with ThreadPoolExecutor(max_workers=self.max_workers) as e:
+            res = {e.submit(self.get_ping_result, target): target for target in self.targets}
+            for future in as_completed(res):
+                self.results.append(future.result())
+
+    def get_active_target_count(self):
+        return len([ r for r in self.results if r.returncode == 0 ])
+
+    def print_stdout(self):
+        for r in self.results:
+            print(r.stdout.decode('utf-8'))
+            
+
+@click.command()
+@click.option('--target', '-t', multiple=True, type=str)
+@click.option('--timeout', '-T', default=1)
+@click.option('--count', '-c', default=3)
+@click.option('--max-workers', default=10)
+def cmd(target, timeout, count):
+    pping = ParallelPing(targets=list(target), timeout=timeout, count=count)
+    pping.run()
+    print(pping.get_active_target_count())
+    pping.print_stdout()
+
+
+def main():
+    cmd()
+
+
+if __name__ == '__main__':
+    main()
